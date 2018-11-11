4515369ad688e2049924c0ea9b394f69e92b16f3
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-11 22:45:52 +0700

    before task 2: random int injection

--- src/main/java/my/spring/CleanerImpl.java
+++ src/main/java/my/spring/CleanerImpl.java
@@ -1,9 +1,13 @@
 package my.spring;
 
 public class CleanerImpl implements Cleaner {
+    private Integer repeat = 2;
+
     @Override
     public void clean() {
         // clean logic
-        System.out.println("VVVVVVVVvvvvvvvvvvvvvvv");
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("VVVVVVVVvvvvvvvvvvvvvvv");
+        }
     }
 }
