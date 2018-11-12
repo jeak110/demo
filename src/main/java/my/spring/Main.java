4fb0ef1d7698f34d80f0dec0a8b04fb55281c59a
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-12 23:15:12 +0700

    before task 4

--- src/main/java/my/spring/Main.java
+++ src/main/java/my/spring/Main.java
@@ -1,7 +1,10 @@
 package my.spring;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        Toolkit.getDefaultToolkit().beep();
         ObjectFactory.getInstance().createObject(IRobot.class).clean();
     }
 }
---
+++ src/main/java/my/spring/PopupSpeaker.java
@@ -0,0 +1,10 @@
+package my.spring;
+
+import javax.swing.*;
+
+public class PopupSpeaker implements Speaker {
+    @Override
+    public void speak(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
